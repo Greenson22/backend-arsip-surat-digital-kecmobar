175a79dcb2c58526c2bc86c9080f47e120711dc2
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-07 17:35:19 +0800

    menambahkan fitur serach di aplikasi incoming_mail

--- arsip_surat_digital/settings.py
+++ arsip_surat_digital/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user_management.User'
 
 REST_FRAMEWORK = {
+    # 'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.SearchFilter'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
--- incoming_mail/views.py
+++ incoming_mail/views.py
@@ -5,27 +5,31 @@ from django.http import Http404
 from .models import IncomingLetter
 from .serializers import IncomingLetterSerializer
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import filters
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 5
     page_size_query_param = 'page_size'
-    # max_page_size = 5
 
 class IncomingLetterList(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['id', 'source', 'recipient', 'agenda_number', 'letter_number', 
+                     'agenda_number', 'letter_date', 'received_date', 'file_url', 'subject']
     pagination_class = StandardResultsSetPagination
 
     def get(self, request, format=None):
-        incoming_letters = IncomingLetter.objects.all()
+        queryset = IncomingLetter.objects.all()
+        filtered_queryset = self.filter_queryset(queryset)
         paginator = self.pagination_class()
-        page = paginator.paginate_queryset(incoming_letters, request)
+        paged_queryset = paginator.paginate_queryset(filtered_queryset, request)
 
-        if page is not None:
-            serializer = IncomingLetterSerializer(page, many=True)
+        if paged_queryset is not None:
+            serializer = IncomingLetterSerializer(paged_queryset, many=True)
             return paginator.get_paginated_response(serializer.data)
         else:
-            serializer = IncomingLetterSerializer(incoming_letters, many=True)
+            serializer = IncomingLetterSerializer(queryset, many=True)
             return Response(serializer.data)
 
     def post(self, request, format=None):
@@ -34,6 +38,11 @@ class IncomingLetterList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def filter_queryset(self, queryset):
+        for backend in list(self.filter_backends):
+            queryset = backend().filter_queryset(self.request, queryset, self)
+        return queryset
 
 class IncomingLetterDetail(APIView):
     authentication_classes = [authentication.TokenAuthentication]
