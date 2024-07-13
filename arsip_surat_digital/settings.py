e66ac6318c6fd152c65dcb1ed3a5bf78f49ecb20
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-13 16:32:01 +0800

    mematikan default pagination

--- arsip_surat_digital/settings.py
+++ arsip_surat_digital/settings.py
@@ -145,8 +145,8 @@ AUTH_USER_MODEL = 'user_management.User'
 
 REST_FRAMEWORK = {
     # 'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.SearchFilter'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 5
 }
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
