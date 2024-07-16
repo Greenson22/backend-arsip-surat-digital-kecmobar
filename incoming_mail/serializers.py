6b5cebf0cbfaab5f05ad797913456b173dfbdedb
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-16 20:17:57 +0800

    perubahan sedikit

--- incoming_mail/serializers.py
+++ incoming_mail/serializers.py
@@ -5,3 +5,8 @@ class IncomingLetterSerializer(serializers.ModelSerializer):
     class Meta:
         model = IncomingLetter
         fields = '__all__'
+
+    # def get_file_url(self, obj):
+    #     if obj.file:
+    #         return obj.file.url
+    #     return None
