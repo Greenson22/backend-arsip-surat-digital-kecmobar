c712cad33b7416bcd4afc6c2790455dd44113728
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-17 21:07:43 +0800

    menambahkan serializers custom

---
+++ api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer # type: ignore
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Tambahkan klaim kustom di sini
+        token['name'] = user.username
+        token['is_superuser'] = user.is_superuser
+        token['groups'] = list(user.groups.values_list('name', flat=True))
+        
+        # Klaim lainnya (opsional)
+        # Contoh:
+        # token['email'] = user.email
+        # token['department'] = user.department  (jika ada field 'department' di model User)
+        return token
