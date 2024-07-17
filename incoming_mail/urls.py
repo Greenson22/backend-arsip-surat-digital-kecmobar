906a4f4aa949f29dd131f8a0cd6bef68398e2ad0
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-17 20:43:23 +0800

    mengubah posisi parameter, awalnya <int:pk>/file menjadi file/<int:pk>

--- incoming_mail/urls.py
+++ incoming_mail/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+
 from incoming_mail.views import IncomingLetterListView, IncomingLetterDetailView, IncomingLetterFileView
 
 urlpatterns = [
     path('', IncomingLetterListView.as_view()),
     path('<int:pk>/', IncomingLetterDetailView.as_view()),
-    path('<int:pk>/file/', IncomingLetterFileView.as_view(), name='incoming-letter-file'),
+    path('file/<int:pk>/', IncomingLetterFileView.as_view(), name='incoming-letter-file'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
