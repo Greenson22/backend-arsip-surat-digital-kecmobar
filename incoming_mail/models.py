5062636d16c4cc9601d9be85ff415c2a198dfa56
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-18 10:02:25 +0800

    mengembailkan pembuatan agenda number otomatis yang salah di nonaktifkan

--- incoming_mail/models.py
+++ incoming_mail/models.py
@@ -15,19 +15,19 @@ class IncomingLetter(models.Model):
     def __str__(self):
         return f"Surat Masuk No. {self.letter_number}"
 
-# from django.db.models.signals import pre_save
-# from django.dispatch import receiver
-# from django.utils.timezone import now
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils.timezone import now
 
-# @receiver(pre_save, sender=IncomingLetter)
-# def generate_agenda_number(sender, instance, **kwargs):
-#     if not instance.agenda_number: #jika agenda number kosong
-#         current_year = now().year
-#         last_letter = IncomingLetter.objects.filter(created_at__year=current_year).order_by('-agenda_number').first()
+@receiver(pre_save, sender=IncomingLetter)
+def generate_agenda_number(sender, instance, **kwargs):
+    if not instance.agenda_number: #jika agenda number kosong
+        current_year = now().year
+        last_letter = IncomingLetter.objects.filter(created_at__year=current_year).order_by('-agenda_number').first()
 
-#         if last_letter: #jika terdapat surat terakhir
-#             last_number = int(last_letter.agenda_number.split('/')[-1])
-#             new_number = last_number + 1
-#         else: #jika surat baru
-#             new_number = 1
-#         instance.agenda_number = f"{current_year}/{new_number:04d}"
+        if last_letter: #jika terdapat surat terakhir
+            last_number = int(last_letter.agenda_number.split('/')[-1])
+            new_number = last_number + 1
+        else: #jika surat baru
+            new_number = 1
+        instance.agenda_number = f"{current_year}/{new_number:04d}"
