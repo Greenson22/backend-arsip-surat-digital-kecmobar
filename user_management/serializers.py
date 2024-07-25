4d5df6e1725469470fffd78c771953583e8cdb1b
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-25 21:19:47 +0800

    membuat agar password hanya bisa di tulis dan menambah fungsi create untuk membuat agar password di hash saat dibuat

--- user_management/serializers.py
+++ user_management/serializers.py
@@ -4,5 +4,15 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = ['id', 'username', 'first_name', 'last_name', 
+		fields = ['id', 'username', 'password', 'first_name', 'last_name', 
 		'email', 'is_active', 'is_superuser', 'date_joined', 'phone_number', 'photo_url']
+		extra_kwargs = {'password': {'write_only': True}}
+	
+	def create(self, validated_data):
+		user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        	)
+		user.set_password(validated_data['password'])
+		user.save()
+		return user
