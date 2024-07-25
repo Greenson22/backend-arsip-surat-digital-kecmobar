e39a07e8125920107044cd1a539ee33fb73c94ac
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-25 21:18:17 +0800

    menghapus id di dalam search fields

--- incoming_mail/views/incomingletter_list_view.py
+++ incoming_mail/views/incomingletter_list_view.py
@@ -16,7 +16,7 @@ class IncomingLetterListView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     
     filter_backends = [filters.SearchFilter]
-    search_fields = ['id', 'source', 'recipient', 'agenda_number', 'letter_number', 'agenda_number', 'letter_date', 'received_date', 'file', 'subject']
+    search_fields = ['source', 'recipient', 'agenda_number', 'letter_number', 'agenda_number', 'letter_date', 'received_date', 'file', 'subject']
     pagination_class = StandardResultsSetPagination
 
     def get(self, request, format=None):
