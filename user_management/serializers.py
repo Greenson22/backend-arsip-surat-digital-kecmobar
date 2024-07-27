752a725f53339f9c78fee35f42c4895d99118b33
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-07-27 09:01:34 +0800

    menyederhanakan pembuatan object user

--- user_management/serializers.py
+++ user_management/serializers.py
@@ -9,10 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 		extra_kwargs = {'password': {'write_only': True}}
 	
 	def create(self, validated_data):
-		user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-        	)
+		user = User.objects.create(**validated_data)
 		user.set_password(validated_data['password'])
 		user.save()
 		return user
