bb3881cb3ee2bfd2d0c0215a7ce1c596419bc433
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-04 13:29:15 +0800

    membuat api untuk mengambil photo profil dari user

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
-from user_management.views import UserViewSet, UserCreateView
+from user_management.views import UserViewSet, UserCreateView, UserProfilePictureView
 
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
 
 urlpatterns = [
      path('', include(router.urls)),
-     path('create/', UserCreateView.as_view())
+     path('create/', UserCreateView.as_view()),
+     path('user/<int:pk>/profile_picture/', UserProfilePictureView.as_view())
 ]
