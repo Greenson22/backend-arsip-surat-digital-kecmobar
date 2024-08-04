2f8b2a2fe87e51f240d4bdf2dd859bdcc3a1906a
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-04 13:33:02 +0800

    menambahkan agar saat update foto profile maka nama dari file yang disimpan didalam server mengikuti aturan yang telah dibuat

--- user_management/views/user_viewset.py
+++ user_management/views/user_viewset.py
@@ -2,7 +2,12 @@ from rest_framework import viewsets, filters, status
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import uuid
+
 from django.http import Http404
+from django.contrib.auth import authenticate
 
 from user_management.models import User
 from user_management.serializers import UserSerializer
@@ -28,6 +33,13 @@ class UserViewSet(viewsets.ViewSet):
         else:
             serializer = UserSerializer(self.queryset, many=True)
             return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk):
         item = self.get_object(pk=pk)
@@ -35,9 +47,23 @@ class UserViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def update(self, request, pk):
+        check = authenticate(request=request, username=request.data.get('username'), password=request.data.get('old_password'))
         item = self.get_object(pk)
+
         serializer = UserSerializer(item, data=request.data)
         if serializer.is_valid():
+            if not check and request.data.get('old_password'):
+                return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+
+            # Handling Photo Upload
+            if 'photo_url' in request.FILES:  # Periksa apakah ada foto yang diunggah
+                photo = request.FILES['photo_url']
+
+                # Buat nama file baru
+                ext = photo.name.split('.')[-1]  # Ambil ekstensi file asli
+                new_filename = f"{slugify(item.username)}-{uuid.uuid4().hex[:6]}.{ext}"  # Nama file baru
+                photo.name = new_filename
+
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
