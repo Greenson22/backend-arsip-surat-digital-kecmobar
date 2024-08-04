ab068a80e908adc66174bda624d187e114660694
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-04 13:28:35 +0800

    membuat agar username dan password saat melakukan put bisa tidak diikutkan

--- user_management/serializers.py
+++ user_management/serializers.py
@@ -1,15 +1,42 @@
 from rest_framework import serializers
 from .models import User
 
+from django.contrib.auth.hashers import make_password
+
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ['id', 'username', 'password', 'first_name', 'last_name', 
 		'email', 'is_active', 'is_superuser', 'date_joined', 'phone_number', 'photo_url']
-		extra_kwargs = {'password': {'write_only': True}}
+		extra_kwargs = {
+			'password': {
+				'write_only': True,
+				'required': False
+			 },
+			 'username': {
+				'required': False
+			 },
+			'photo_url': {'write_only': True}}
 	
 	def create(self, validated_data):
 		user = User.objects.create(**validated_data)
 		user.set_password(validated_data['password'])
 		user.save()
 		return user
+
+	def update(self, instance, validated_data):
+		instance.__dict__.update(validated_data) 
+
+		password = validated_data.get('password', None)
+		if password:
+			instance.password = make_password(password)
+		
+		instance.save()
+		return instance
+
+class UserProfilePictureSerializer(serializers.ModelSerializer):
+	# photo_url = serializers.ImageField(use_url=False)
+	
+	class Meta:
+		model = User
+		fields = ['photo_url']
