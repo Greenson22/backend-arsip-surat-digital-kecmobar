f9e1391aad73e18b78f7ef67075012c6fa5e8e0c
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-21 22:25:15 +0800

    refactor : mempercantik kode

--- arsip_surat_digital/urls.py
+++ arsip_surat_digital/urls.py
@@ -2,18 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('incoming_mail/', include('incoming_mail.urls')),
+    path('outgoing_mail/', include('outgoing_mail.urls')),
     path('users/', include('user_management.urls')),
-]
-
-urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
-]
-
-urlpatterns += [
-     path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
 ]
