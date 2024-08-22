41ce4a9b6aff8bb881557e960f73cc2835cfaf39
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-22 10:36:41 +0800

    refactor : menghapus baris kode yang tidak perlu

--- user_management/views/user_create_view.py
+++ user_management/views/user_create_view.py
@@ -1,14 +1,10 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.authentication import JWTAuthentication # type: ignore
 
 from user_management.models import User
 from user_management.serializers import UserSerializer
 
 class UserCreateView(generics.CreateAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
