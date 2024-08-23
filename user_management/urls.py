a0c56d69c655761866be5e4528981a8490f21a3a
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-23 18:56:39 +0800

    refactor : menbamhakan user password viewset

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
-from user_management.views import UserViewSet, UserCreateView, UserProfilePictureView
+from user_management.views import UserViewSet, UserProfilePictureView, UserPasswordViewSet
 
 router = routers.DefaultRouter()
-router.register('', UserViewSet)
+router.register('', UserViewSet, basename='user')
+router.register('password', UserPasswordViewSet, basename='user-password')
 
 urlpatterns = [
-     path('create/', UserCreateView.as_view()),
-     path('user/<int:pk>/profile_picture/', UserProfilePictureView.as_view())
+     path('<int:pk>/profile_picture/', UserProfilePictureView.as_view())
 ]
 
 urlpatterns += router.urls
