1bfd0052beb3c84f7aa5a3e6589a773f874359cb
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-03 08:19:16 +0800

    refactor :  menambahkan url genai

--- arsip_surat_digital/urls.py
+++ arsip_surat_digital/urls.py
@@ -5,6 +5,7 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('genai/', include('genai.urls')),
     path('incoming_mail/', include('incoming_mail.urls')),
     path('outgoing_mail/', include('outgoing_mail.urls')),
     path('users/', include('user_management.urls')),
