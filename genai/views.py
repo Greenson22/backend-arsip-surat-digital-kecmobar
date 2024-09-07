8cd1b8a5665039685792ac6d3b9d37db37ae8756
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-08 07:31:08 +0800

    refactor : menambahkan multiple extract letter entities view

--- genai/views.py
+++ genai/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import StreamingHttpResponse
 import json
 import tempfile
 import os
@@ -9,12 +10,19 @@ from .mygenai import system_intructions
 
 model = MyGenAi(system_intructions['incomingmail'])
 model2 = MyGenAi(system_intructions['summary'])
+import time
 
 class ExtractLetterEntitiesView(APIView):
      def post(self, request):
           global model
-          response = process_file_with_model( request.FILES['file'], model)
-          return Response(json.loads(response))
+          file = request.FILES['file']
+          print('perminataan')
+          data = json.loads(model.generate_content_file(file))
+          response = {
+               'name' : file.name,
+               'entities' : data
+               }
+          return Response(response)
      
 class SummarizeLetterView(APIView):
      def post(self, request):
@@ -22,11 +30,22 @@ class SummarizeLetterView(APIView):
           response = process_file_with_model(request.FILES['file'], model2)
           return Response(json.loads(response))
      
+class MultipleExtractLetterEntitiesView(APIView):
+     def post(self, request):
+          files = request.FILES.getlist('file')
+          with tempfile.TemporaryDirectory() as temp_dir:
+               for file in files:
+                    temp_file_path = os.path.join(temp_dir, file.name)
+                    with open(temp_file_path, 'wb') as temp_file:
+                        for chunk in file.chunks():
+                            temp_file.write(chunk)
+          return Response({})
+     
 def process_file_with_model(file, model):
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_file_path = os.path.join(temp_dir, file.name)
         with open(temp_file_path, 'wb') as temp_file:
             for chunk in file.chunks():
                 temp_file.write(chunk)
-        response = model.send_message_file(temp_file_path)
+        response = model.generate_content(temp_file_path)
     return response
