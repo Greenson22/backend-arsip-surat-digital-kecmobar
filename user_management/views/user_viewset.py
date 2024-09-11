b6add7a8cc2fd8c64b383d6a191de671227f5c71
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-09-11 20:30:29 +0800

    refactor : menghapus mixins

--- user_management/views/user_viewset.py
+++ user_management/views/user_viewset.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 from ..models import User
 from ..serializers import UserSerializer
 from .model_mixin import ListCustom, CreateCustom, RetrieveCustom, UpdateCustom, DestroyCustom
