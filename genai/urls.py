8b20653b7dcfa62833fdd297f00e1b49d5518a9b
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-10-26 14:45:37 +0800

    refactor : menambahkan endpoint untuk letter classification

--- genai/urls.py
+++ genai/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from genai.views import ExtractLetterEntitiesView, SummarizeLetterView, LetterOCRView
+from genai.views import ExtractLetterEntitiesView, SummarizeLetterView, LetterOCRView, LetterClassificationView
 
 urlpatterns = [
      path('extract_letter_entities/', ExtractLetterEntitiesView.as_view()),
      path('letter_ocr/', LetterOCRView.as_view()),
+     path('letter_classification/', LetterClassificationView.as_view()), 
      path('summarize_letter/', SummarizeLetterView.as_view())
 ]
