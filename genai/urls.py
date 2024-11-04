0c6440aba9460db9488b8b6b5591de951ecc70b2
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-11-04 19:31:07 +0800

    refactor : perubahan url endpoint

--- genai/urls.py
+++ genai/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from genai.views import ExtractLetterEntitiesView, SummarizeLetterView, LetterOCRView, LetterClassificationView
+from genai.views import ExtractLetterEntitiesView, SummarizeLetterView, LetterOCRView, LetterLocalClassification
 
 urlpatterns = [
      path('extract_letter_entities/', ExtractLetterEntitiesView.as_view()),
      path('letter_ocr/', LetterOCRView.as_view()),
-     path('letter_classification/', LetterClassificationView.as_view()), 
+     path('classify_letter/', LetterLocalClassification.as_view()),
      path('summarize_letter/', SummarizeLetterView.as_view())
 ]
